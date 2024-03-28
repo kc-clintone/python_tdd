f1f2c0012910457e2d7f1739690cd3abc65edcae
kc-clintone <kayseeclintone@gmail.com>
2024-03-28 17:38:04 +0000

    python tdd >>> writing tests

--- tests/test_testables.py
+++ tests/test_testables.py
@@ -19,4 +19,16 @@ class DocTestDouble:
     """
     def __init__(self, sent, ents):
         self.ents = [SpanTestDouble(ent['text'], ent['label_'] for ent in ents)]
-        
+
+    def patch_method(self, attr, return_value):
+        def patched(): return return_value
+        setattr(self, attr, patched)
+        return self
+
+class SpanTestDouble:
+    """
+    SpanTestDouble class.
+    """
+    def __init__(self, text, label):
+        self.text = text
+        self.label_ = label
