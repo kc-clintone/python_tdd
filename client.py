0465d5bc97cc8f253dabd2f0cf7fde043bf87f4b
kc-clintone <kayseeclintone@gmail.com>
2024-03-28 19:39:57 +0000

    python tdd >>> writing tests

--- client.py
+++ client.py
@@ -5,4 +5,17 @@ class NamedEntityClient():
         self.model = model
 
     def get_entity(self, sentence):
+        doc = self.model(sentence)
+        entities = [{'ents': ent.text, 'label_': self.map_label(ent.label)} for ents in doc.ents]
         return {'ents': [], 'html': ''}
+    
+    @staticmethod
+    def map_label(label):
+        this_name = {
+            'PERSON' : 'person',
+            'NORP': 'Group',
+            'GPE': 'Location',
+            'LANGUAGE': 'Language',
+            'LOC': 'Location'
+        }
+    return this_name.get(label)
--- tests/test_client.py
+++ tests/test_client.py
@@ -26,3 +26,51 @@ class TestClient(unittest.TestCase):
         entity = nec.get_entity("...")
         results = {'ents': ['ent':'Beyonce', 'label':'person'], html: ""}
         self.assertListEqual(ents['ents'], results['ents'])
+
+    def test_get_ents_given_spacy_NORP_is_returned(self):
+      │ model = NecModelTestDouble("eng")
+      │ doc_ents = [{'text':'Kenyan', 'label_':'NORP'}]
+      │ model.returns_doc_ents(doc_ents)
+      │ nec = NamedEntityClient(model)
+      │ entity = nec.get_entity("...")
+      │ results = {'ents': ['ent':'Kenyan', 'label':'Group'], html: "
+        self.assertListEqual(ents['ents'], results['ents'])
+
+    def test_get_ents_given_spacy_LOC_is_returned(self):
+        model = NecModelTestDouble("eng")
+        doc_ents = [{'text':'At the beach', 'label_':'LOC'}]
+        model.returns_doc_ents(doc_ents)
+        nec = NamedEntityClient(model)
+        entity = nec.get_entity("...")
+        results = {'ents': ['ent':'At the beach', 'label':'Location'], html: "
+        self.assertListEqual(ents['ents'], results['ents'])
+
+    def test_get_ents_given_spacy_LANGUAGE_is_returned(self):
+    │   model = NecModelTestDouble("eng")
+        doc_ents = [{'text':'ASL', 'label_':'LANGUAGE'}]
+        model.returns_doc_ents(doc_ents)
+        nec = NamedEntityClient(model)
+        entity = nec.get_entity("...")
+        results = {'ents': ['ent':'ASL', 'label':'Language'], html: "
+        self.assertListEqual(ents['ents'], results['ents'])
+
+    def test_get_ents_given_spacy_GPE_is_returned(self):
+        model = NecModelTestDouble("eng")
+        doc_ents = [{'text':'Nairobi', 'label_':'GPE'}]
+        model.returns_doc_ents(doc_ents)
+        nec = NamedEntityClient(model)
+        entity = nec.get_entity("...")
+        results = {'ents': ['ent':'Nairobi', 'label':'Location'], html: "
+        self.assertListEqual(ents['ents'], results['ents'])
+
+def test_get_ents_given_spacy_all_is_returned(self):
+   6   │   │   model = NecModelTestDouble("eng")
+5   │   │   doc_ents = [{'text': 'New York', 'label_': 'GPE'},
+                        {'text':'Cardi B', 'label_':'PERSON'}]
+   4   │   │   model.returns_doc_ents(doc_ents)
+   3   │   │   nec = NamedEntityClient(model)
+   2   │   │   entity = nec.get_entity("...")
+   1   │   │   results = {'ents': 
+                         [{'ent':'New York', 'label':'Location'},
+                          {'ent': 'Cardi B', 'label': 'person'}], html: "
+  28   │   │   self.assertListEqual(ents['ents'], results['ents'])
