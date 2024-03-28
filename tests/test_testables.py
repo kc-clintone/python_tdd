08f6d4969fec35fc3064ec1457e730d2c0785dc4
kc-clintone <kayseeclintone@gmail.com>
2024-03-28 17:26:29 +0000

    python tdd >>> writing tests

--- tests/test_testables.py
+++ tests/test_testables.py
@@ -3,4 +3,20 @@ class NecModelTestables:
     Test clss for Ner models
     """
 
-    
+def __init__(self, model):
+    self.model = model
+
+def returns_doc_ents(self ents):
+    self.ents = ents
+
+def __call__(self, sent):
+    return DocTestDouble(sent, self.ents)
+
+
+class DocTestDouble:
+    """
+    Test class for DocTestDouble
+    """
+    def __init__(self, sent, ents):
+        self.ents = [SpanTestDouble(ent['text'], ent['label_'] for ent in ents)]
+        
